b3f3bbc17e0c8d65dfc1af72e3be12b0857f1d49
Nohcnadib <nohcnadib@gmail.com>
2024-10-28 17:17:48 -0300

    Pre deploy

--- src/App.js
+++ src/App.js
@@ -34,16 +34,7 @@ function App() {
             </svg>
             Torneo
           </li>
-          <li
-            className={`flex p-4 justify-center gap-2 cursor-pointer ${selected === 'historialTorneos' ? 'bg-gray-700' : 'hover:bg-gray-700'}`}
-            onClick={() => setSelected('historialTorneos')}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-              <path fill-rule="evenodd" d="M15.988 3.012A2.25 2.25 0 0 1 18 5.25v6.5A2.25 2.25 0 0 1 15.75 14H13.5V7A2.5 2.5 0 0 0 11 4.5H8.128a2.252 2.252 0 0 1 1.884-1.488A2.25 2.25 0 0 1 12.25 1h1.5a2.25 2.25 0 0 1 2.238 2.012ZM11.5 3.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 .75.75v.25h-3v-.25Z" clip-rule="evenodd" />
-              <path fill-rule="evenodd" d="M2 7a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7Zm2 3.25a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1-.75-.75Zm0 3.5a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
-            </svg>
-            Historial Torneos
-          </li>
+          
           <li
             className={`flex p-4 justify-center gap-2 cursor-pointer ${selected === 'historialEquipos' ? 'bg-gray-700' : 'hover:bg-gray-700'}`}
             onClick={() => setSelected('historialEquipos')}
